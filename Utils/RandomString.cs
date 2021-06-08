0b63593f93e189e0471bac90275b394161b0839f
F34th3R <jafetlopezch@gmail.com>
2021-06-08 16:27:20 -0600

    RandomString and .gitignore updated

--- Utils/RandomString.cs
+++ Utils/RandomString.cs
@@ -8,7 +8,7 @@ namespace TodoApp.Utils
         public static string GenerateRandomString(int length)
         {
             var random = new Random();
-            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             return new string(Enumerable.Repeat(chars, length)
                 .Select(x => x[random.Next(x.Length)]).ToArray());
         }
